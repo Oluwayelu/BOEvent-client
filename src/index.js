5f975ab79899ad303e96e7ee2dc307c1e2d0e9cb
oluwayelu <oluwayeluifeoluwa@gmail.com>
2023-07-10 01:12:11 +0100

    fix: remove .env file

--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:8000/',
-  // uri: process.env.REACT_GRAPHQLAPI_URL,
+  uri: process.env.REACT_APP_GRAPHQLAPI_URL,
   cache: new InMemoryCache(),
 });
 
