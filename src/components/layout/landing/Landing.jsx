7c42a0ad6317addff2e4981d38a4b946ae4dd2f6
oluwayelu <oluwayeluifeoluwa@gmail.com>
2023-08-02 20:08:53 +0100

    fix: added option to include footer

--- src/components/layout/landing/Landing.jsx
+++ src/components/layout/landing/Landing.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 import { Footer, Navbar } from 'components/modules';
 
-const Landing = ({ children }) => {
+const Landing = ({ children, footer = true }) => {
   return (
     <div className="relative">
       {/* navbar */}
@@ -10,13 +10,14 @@ const Landing = ({ children }) => {
       {/* content */}
       <div className="min-h-[90vh]">{children}</div>
       {/* footer */}
-      <Footer />
+      {footer && <Footer />}
     </div>
   );
 };
 
 Landing.propTypes = {
   children: PropTypes.node,
+  footer: PropTypes.bool,
 };
 
 export default Landing;
